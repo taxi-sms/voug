1977a7b366365e41aa3777130a8dd826948d7cb7
TSMs <tsubakiharanaoya1203@gmail.com>
2026-02-28 03:30:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'invoice-pwa-htmlpdf-v5';
+const CACHE_NAME = 'invoice-pwa-htmlpdf-v4';
 const CORE_ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './offline.html'
+  './offline.html',
+  './VOUG-IV-SS.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png',
+  './apple-touch-icon.png',
+  './favicon-32.png',
+  './favicon-16.png'
 ];
 
 self.addEventListener('install', (event) => {
